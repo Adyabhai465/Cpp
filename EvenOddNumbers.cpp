0d124692509db806b79a5c16ef61f51a10404c8b
Aditya <138976123+Adyabhai465@users.noreply.github.com>
2024-06-14 09:24:02 +0530

    Create EvenOddNumbers.cpp

---
+++ EvenOddNumbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int n;
+    cin>>n;
+    if (n%2==0)
+        cout<<"The Number is even.";
+    else
+        cout<<"The Number is odd.";  
+}
