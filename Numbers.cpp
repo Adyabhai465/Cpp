007ec5bf6f6ac30d769e2ce2822bf91dd87c7d59
Aditya <138976123+Adyabhai465@users.noreply.github.com>
2024-06-19 20:18:59 +0530

    Add files via upload

---
+++ Numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cout<<"Enter the starting number : ";
+    cin>>a;
+    cout<<"Enter the last number : ";
+    cin>>b;
+    for (int i= a ; i<=b ; i++){
+        cout<<i<<endl;
+    }
+}
